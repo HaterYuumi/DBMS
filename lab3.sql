a649778b884e0d219b2d6121f6ba6af35bace5a3
HaterYuumi <114468482+HaterYuumi@users.noreply.github.com>
2025-09-16 22:34:53 +0700

    Update lab3.sql

--- lab3.sql
+++ lab3.sql
@@ -1,18 +1,35 @@
---SELECT * FROM employees;
---	SELECT  e.last_name ||' '|| e.first_name AS employee, e.hire_date, j.job_title,
---	    EXTRACT(DAY FROM (DATE_TRUNC('MONTH', hire_date) + INTERVAL '1 MONTH' - INTERVAL '1 DAY' - hire_date)) AS days_until_month_end	
---	FROM employees e INNER JOIN jobs j ON e.job_id = j.job_id
---	WHERE EXTRACT(DAY FROM (DATE_TRUNC('MONTH', e.hire_date) + INTERVAL '1 MONTH' - INTERVAL '1 DAY' - e.hire_date)) <= 10;
+-- 1 --
+SELECT last_name ||' '|| UPPER(SUBSTR(first_name, 1, 1)), 
+	hire_date	
+FROM employees WHERE date_part('day', hire_date) > 20;
+-- 2 --
+SELECT last_name ||' '|| UPPER(SUBSTR(first_name, 1, 1)), 
+	EXTRACT(JULIAN FROM hire_date) AS JD_hire_date	
+	FROM employees;
+-- 3 --
+SELECT last_name ||' - '|| phone_number, 
+	to_char(hire_date, 'DD MONTH YYYY'),
+	(current_date - hire_date)/30 AS work_month
+FROM employees;
+-- 4 --
+SELECT first_name ||' '|| last_name, 
+	hire_date FROM employees 
+WHERE (date_part('year', hire_date) BETWEEN 1990 AND 1995)
+	AND (current_date - hire_date)/365 > 30;
+-- 5 --
+SELECT UPPER(last_name) ||' '|| hire_date,
+	to_char(hire_date, 'DAY - MONTH') AS hire_date2 FROM employees
+WHERE (job_id BETWEEN 14 AND 19) OR
+	salary BETWEEN 9000 AND 25000;
+-- 6 --
+SELECT last_name, 
+	to_char(hire_date, 'DD - YYYY') FROM employees
+WHERE date_part('month', hire_date) = 1;
+-- 7 --
+SELECT last_name, 
+	date_trunc('day', hire_date) AS hire_day FROM employees
+WHERE EXTRACT(DAY FROM hire_date) BETWEEN 15 AND 31;
 
---SELECT last_name ||' '|| UPPER(SUBSTR(first_name, 1, 1)), 
---	hire_date	
---	FROM employees WHERE date_part('day', hire_date) > 20;
-
---SELECT last_name ||' '|| UPPER(SUBSTR(first_name, 1, 1)), 
---	EXTRACT(JULIAN FROM hire_date) AS JD_hire_date	
---	FROM employees;
-
---SELECT last_name ||' - '|| phone_number, 
---	to_char(hire_date, 'DD MONTH YYYY'),
---	(current_date - hire_date)/30 AS work_month
---FROM employees;
+SELECT last_name, 
+	date_trunc('day', hire_date) AS hire_day FROM employees
+WHERE EXTRACT(DAY FROM hire_date) BETWEEN 15 AND 31;
